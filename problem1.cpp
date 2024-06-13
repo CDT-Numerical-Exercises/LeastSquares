b2f11bd3fc14a4ed4a0780600761634ef7d3fd9b
Joshua Barrass <josh.barrass.work@gmail.com>
2024-06-13 16:03:23 +0100

    Load CSV in main

--- problem1.cpp
+++ problem1.cpp
@@ -4,10 +4,21 @@
 #include "csv.h"
 #include "helpers.h"
 
-int main() {
-  gsl_matrix *m = load_csv_to_dmatrix("test.csv");
-  std::cout << m->size1 << "x" << m->size2 << std::endl;
-  print_matrix(m);
-  gsl_matrix_free(m);
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "Requires path to CSV file as arg." << std::endl;
+    return 1;
+  }
+
+  // load the data
+  // we know in advance that this will be a single column, so we can
+  // make it into a vector
+  gsl_matrix *Y = load_csv_to_dmatrix(argv[1]);
+  gsl_vector_view y_view = gsl_matrix_column(Y, 0);
+  gsl_vector *y = &y_view.vector;
+
+  print_vector(y);
+  
+  gsl_matrix_free(Y);
   return 0;
 }
